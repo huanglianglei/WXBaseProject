718368419a44ca64081e4213fe8f70075f092b8a
huanglianglei <461031544@qq.com>
2020-09-25 16:59:51 +0800

    第一次提交代码

---
+++ BaseProject/utils/api.js
@@ -0,0 +1,9 @@
+const  ApiBaseUrl = 'http://zszc.hanyu365.com.cn:8080/';
+module.exports = {
+  ApiBaseUrl,
+  ListDataApi: ApiBaseUrl + 'zszc/app_recommend',
+  HomeDataApi: ApiBaseUrl + 'zszc/app_home/getHome'
+};
+
+
+
---
+++ BaseProject/utils/httpRequest.js
@@ -0,0 +1,50 @@
+/**
+ * GET请求
+ */
+function get(url,data={}){
+  return request(url,data,'GET');
+}
+/**
+ * POST请求
+ */
+function post(url,data={}){
+  return request(url,data,'POST');
+}
+
+/**
+ * 微信的Request网络请求
+ */
+function request(url,data = {},method = "GET"){
+  var contentType = 'application/json'
+  return new Promise(function(resolve,reject){
+    wx.request({
+      url: url,
+      data:data,
+      method:method,
+      header:{
+        'Content-Type':contentType
+      },
+      success:(res)=>{
+        if (res.statusCode == 200) {
+          if(res.data.response == 0){
+            resolve(res.data);
+          }
+          else{
+            reject(res.data.desc);
+          }
+        }
+        else{
+          reject("请求失败：" + res.statusCode);
+        }
+      },fail:(error)=>{
+        reject("服务器连接异常，请检查网络再试");
+      }
+    })
+  });
+
+}
+module.exports = {
+  request,
+  get,
+  post
+}
